cc64c78ab9388d35698d7c92e6bf77d73cc831b9
Matt Van Horn <mattvh@scoopst.com>
2012-01-07 10:33:26 -0800

    my brand new Rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Placepanda::Application.config.secret_token = '0fd2eae7c74a6acec76ac74e4f92813eee26eb0e28a8c0e217b070c3ac3b9aebde34ff77fcd2031638da511f7b59470d476432b0707308b901872dc00799f2fe'
